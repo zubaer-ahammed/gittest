c4c520f81b3b23d19e1f3a60ec23e0fb1f084e2c
Md. Zubaer Ahammed <zubaerahammed223@gmail.com>
2016-01-05 00:50:12 +0600

    updated script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var numbers=[1,2,3,4,5,6];
 //calculate summation of the numbers in an array
 function sum(n){
 	var summation = 0;
-
+	summation = n.reduce(function(p,c){return p+c;});
 	return summation;
 }
 
